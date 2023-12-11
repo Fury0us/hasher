38df0d5ff5b1c323717f47993122b006d2b4e542
fury0us <139115148+Fury0us@users.noreply.github.com>
2023-12-11 22:18:58 +0000

    Update hasher.sh
    added multiple new encryption algorithms such as:
    Serpent
    Twofish
    ARIA
    RSA
    Rijndael

--- hasher.sh
+++ hasher.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 #the goal of this script is to convert text to hash automatically and vice versa
 #for possible password recovery or encoding on the go
+if [ /usr/bin/mcrypt ]
+then
+        echo "mcrypt is installed.."
+else
+        sudo apt install mcrypt &&
+        clear
+fi
 main_menu() {
 COLUMNS=12
 echo $'\e[1;31m'
@@ -13,35 +20,79 @@ else
 	clear &&
 	figlet HASHER
 fi
-echo '* ' "means that it is a TWO WAY hashing algorythm"
+echo ' - ' "means that it is a TWO WAY hashing algorythm"
+echo '-------------------------------------------------'
+echo ""
+
 COLUMNS=12
 PS3=("#main: ")
-hasher=("*gnuprivacygpg" "*openssl_base64" "openssl_sha1sum" "openssl_sha256sum" "openssl_sha512sum" "*base_64" "exit")
+hasher=("-AES" "-Arcfour" "-ARIA" "-RSA" "-Rijndael" "-Serpent" "-Twofish" "-gnuprivacygpg" "-openssl_base64" "openssl_sha1sum" "openssl_sha256sum" "openssl_sha512sum" "-base_64" "exit")
 select hash in "${hasher[@]}"; do
 case $hash in
-"*gnuprivacygpg")
-clear
-gnuprivacygpg
+"-AES")
+echo -en '\n'
+aes
+echo -en '\n'
 ;;
-"*openssl_base64")
-clear
+"-Arcfour")
+echo -en '\n'
+arcfour
+echo -en '\n'
+;;
+"-ARIA")
+echo -en '\n'
+aria
+echo -en '\n'
+;;
+"-RSA")
+echo -en '\n'
+rsa_enc
+echo -en '\n'
+;;
+"-Rijndael")
+echo -en '\n'
+rijn
+echo -en '\n'
+;;
+"-Serpent")
+echo -en '\n'
+serpent
+echo -en '\n'
+;;
+"-Twofish")
+echo -en '\n'
+twofish
+echo -en '\n'
+;;
+"-gnuprivacygpg")
+echo -en '\n'
+gnuprivacygpg
+echo -en '\n'
+;;
+"-openssl_base64")
+echo -en '\n'
 openssl_base64
+echo -en '\n'
 ;;
 "openssl_sha1sum")
-clear
+echo -en '\n'
 openssl_sha1sum
+echo -en '\n'
 ;;
 "openssl_sha256sum")
-clear
+echo -en '\n'
 openssl_sha256sum
+echo -en '\n'
 ;;
 "openssl_sha512sum")
-clear
+echo -en '\n'
 openssl_sha512sum
+echo -en '\n'
 ;;
-"*base_64")
-clear
+"-base_64")
+echo -en '\n'
 base_64
+echo -en '\n'
 ;;
 "exit")
 clear
@@ -52,6 +103,180 @@ exit
 	sleep 1
 	clear
 	main_menu
+esac
+done
+}
+aes() {
+COLUMNS=12
+read -p $'\e[31mKeySize?(128,192,256): \e[0m' keysize
+read -p $'\e[31mecb, cbc, ofb, ctr, cfb8, cbf1, cfb: \e[0m' var0
+echo "REMEMBER YOUR CHOICES!"
+PS3=("#aes: ")
+aesk=("Encrypt" "Decrypt" "Main")
+select ae in "${aesk[@]}"; do
+case $ae in
+"Encrypt")
+read -p $'\e[31mMessage?: \e[0m' encmsg
+read -p $'\e[31mPass?: \e[0m' pass
+read -p $'\e[31mFileToWrite?: \e[0m' outfile
+echo $encmsg | openssl enc -e -k $pass -aes-$keysize-$var0 -base64 -iter 20 > $outfile
+;;
+"Decrypt")
+read -p $'\e[31mPass?: \e[0m' passw
+read -p $'\e[31mfile?: \e[0m' infile
+sudo openssl enc -d -k $passw -in $infile -aes-$keysize-$var0 -a -iter 20
+;;
+"Main")
+clear
+main_menu
+;;
+esac
+done
+}
+arcfour() {
+COLUMNS=12
+PS3=("#arcf: ")
+arcfour=("Encrypt" "Decrypt" "Main")
+select arc in "${arcfour[@]}"; do
+case $arc in
+"Encrypt")
+echo "you must first make a file to encrypt"
+read -p $'\e[31mName of file?: \e[0m' fname
+touch $fname
+read -p $'\e[31mMessage to encrypt?: \e[0m' encmsg
+echo $encmsg > $fname
+sudo mcrypt -a arcfour -b $fname &&
+sudo shred $fname &&
+sudo rm $fname
+echo "your file has been encrypted, and the original copy has been"
+echo "obfuscated and deleted. you can find you file with a new extension"
+;;
+"Decrypt")
+read -p $'\e[31mFiletoDecrypt?: \e[0m' decfile
+sudo mcrypt -d -a arcfour -b $decfile
+;;
+"Main")
+clear
+main_menu
+;;
+esac
+done
+}
+aria() {
+COLUMNS=12
+read -p $'\e[31mKeySize?(128,192,256): \e[0m' keysize
+read -p $'\e[31mecb, cbc, ofb, ctr, cfb8, cbf1, cfb: \e[0m' var0
+echo "REMEMBER YOUR CHOICES!"
+PS3=("#aria: ")
+ariaa=("Encrypt" "Decrypt" "Main")
+select ar in "${ariaa[@]}"; do
+case $ar in
+"Encrypt")
+read -p $'\e[31mMessage?: \e[0m' encmsg
+read -p $'\e[31mPass?: \e[0m' pass
+read -p $'\e[31mFileToWrite?: \e[0m' outfile
+echo $encmsg | openssl enc -e -k $pass -aria-$keysize-$var0 -a -iter 20 > $outfile
+;;
+"Decrypt")
+read -p $'\e[31mPass?: \e[0m' passw
+read -p $'\e[31mfile?: \e[0m' infile
+sudo openssl enc -d -k $passw -in $infile -aria-$keysize-$var0 -a -iter 20
+;;
+"Main")
+clear
+main_menu
+;;
+esac
+done
+}
+rijn() {
+COLUMNS=12
+PS3=("#rijn: ")
+rijinnc=("Encrypt" "Decrypt" "Main")
+select ri in "${rijinnc[@]}"; do
+case $ri in
+"Encrypt")
+echo "you must first make a file to encrypt"
+read -p $'\e[31mName of file?: \e[0m' fname
+touch $fname
+read -p $'\e[31mMessage to encrypt?: \e[0m' encmsg
+echo $encmsg > $fname
+read -p $'\e[31mHash Mode?(cbc or ctr): \e[0m' hmode
+sudo mcrypt -a rijndael-256 -b -m $hmode $fname &&
+sudo shred $fname &&
+sudo rm $fname
+echo "you file has been encrypted, and the original copy has been"
+echo "obfuscated and deleted. you can find you file with a new extension"
+;;
+"Decrypt")
+read -p $'\e[31mFiletoDecrypt?: \e[0m' decfile
+sudo mcrypt -d -a rijndael-256 -b $decfile
+;;
+"Main")
+clear
+main_menu
+;;
+esac
+done
+}
+rsa_enc() {
+echo "generating rsa pubkey for encryption and decoding"
+read -p $'\e[31mName of private keyfile?: \e[0m' keyname
+read -p $'\e[31mName of public keyfile?: \e[0m' key
+sudo openssl genpkey -algorithm RSA -out $keyname.pem -aes256
+sudo openssl rsa -pubout -in $keyname.pem -out $key.pem
+echo "you need these files for rsa encryption and decryption"
+echo "but delete them when you are done with transmission"
+echo "REMEMBER THE PASSWORD YOU SET OR YOU DATA IS GONE FOREVER"
+COLUMNS=12
+PS3=("#rsa: ")
+rsa=("Encrypt" "Decrypt" "Main")
+select rs in "${rsa[@]}"; do
+case $rs in
+"Encrypt")
+read -p $'\e[31mMessage?: \e[0m' encmsg
+read -p $'\e[31mOutfile?: \e[0m' outfile
+echo -n $encmsg | sudo openssl rsautl -encrypt -pubin -inkey $key.pem -out $outfile
+;;
+"Decrypt")
+read -p $'\e[31mEncrypted File?: \e[0m' infile
+sudo openssl rsautl -decrypt -inkey $keyname.pem -in $infile
+;;
+"Main")
+clear
+main_menu
+;;
+esac
+done
+}
+serpent() {
+COLUMNS=12
+PS3=("#serp: ")
+serrrp=("Encrypt" "Decrypt" "Main")
+select serr in "${serrrp[@]}"; do
+case $serr in
+"Encrypt")
+echo "you must first make a file to encrypt"
+read -p $'\e[31mName of file?: \e[0m' fname
+touch $fname
+read -p $'\31mMessage to Encrypt?: \e[0m' encmsg
+echo $encmsg > $fname
+read -p $'\e[31mHash Mode?(cbc or ctr): \e[0m' hmode
+sudo mcrypt -a serpent -b -m $hmode $fname &&
+sudo shred $fname &&
+sudo rm $fname
+echo "your file has been encrypted, and the original copy has been"
+echo "obfuscated and deleted. you can find you file with a new extension"
+;;
+"Decrypt")
+read -p $'\e[31mFiletoDecrypt?: \e[0m' decfile
+sleep 0.5
+sudo mcrypt -d -a serpent -b $decfile
+;;
+"Main")
+clear
+main_menu
+;;
 esac
 done
 }
@@ -115,8 +340,6 @@ echo -n $encmsg | openssl enc -base64
 fi
 ;;
 "Decode")
-echo "if just text enter double quotes around message"
-echo "if symbols enter single quotes around message"
 read -p $'\e[31mEncrypted Text?: \e[0m' dect
 echo $dect | openssl enc -d -a
 ;;
@@ -204,10 +427,39 @@ echo -n $encmsg | base64
 fi
 ;;
 "Decode")
-echo "if just text enter double quotes around message"
-echo "if symbols enter single quotes around message"
 read -p $'\e[31mEncrypted Text?: \e[0m' dect
 echo $dect | base64 -d
+;;
+"Main")
+clear
+main_menu
+;;
+esac
+done
+}
+twofish() {
+COLUMNS=12
+PS3=("#twofsh: ")
+twofi=("Encrypt" "Decrypt" "Main")
+select tw in "${twofi[@]}"; do
+case $tw in
+"Encrypt")
+echo "you must first make a file to encrypt"
+read -p $'\e[31mName of file?: \e[0m' fname
+touch $fname
+read -p $'\31mMessage to Encrypt?: \e[0m' encmsg
+echo $encmsg > $fname
+read -p $'\e[31mHash Mode?(cbc or ctr): \e[0m' hmode
+sudo mcrypt -a twofish -b -m $hmode $fname &&
+sudo shred $fname &&
+sudo rm $fname
+echo "your file has been encrypted, and the original copy has been"
+echo "obfuscated and deleted. you can find you file with a new extension"
+;;
+"Decrypt")
+read -p $'\e[31mFiletoDecrypt?: \e[0m' decfile
+sleep 0.5
+sudo mcrypt -d -a twofish -b $decfile
 ;;
 "Main")
 clear
